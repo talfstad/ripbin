9e3cea7ec805ac1cd7d91df2219de3d6cc66a111
Trevor Alfstad <trevor@buildcave.com>
2016-12-13 23:40:28 -0800

    travis-ci config

--- ci/invalidate.js
+++ ci/invalidate.js
@@ -2,6 +2,8 @@ var cloudfront = require("./cloudfront");
 var config = require("./config");
 
 var branch = process.argv[2]
+console.log("travis CI branch: " + branch);
+
 if (branch == "test" || branch == "prod") {
 
   cloudfront.invalidate(branch);
